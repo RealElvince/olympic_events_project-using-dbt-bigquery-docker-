33273719de3e8dd37d46b73eafaeda513c29b660
RealElvince <obuongely@yahoo.com>
2025-06-23 13:11:48 +0300

    create table function update

--- bigquery_dataset/table.py
+++ bigquery_dataset/table.py
@@ -1,27 +1,15 @@
 from google.cloud import bigquery
-from dotenv import load_dotenv
-import os
 import logging
 from google.api_core.exceptions import NotFound
 
-load_dotenv()
+key_file = "/opt/airflow/gcp/service_account.json"
+client = bigquery.Client.from_service_account_json(key_file)
 
-PROJECT_ID = os.getenv("PROJECT_ID")
-DATASET_NAME = os.getenv("DATASET_NAME")
-TABLE_NAME = os.getenv("TABLE_NAME")
-KEY_FILE = "/opt/airflow/gcp/service_account.json"
-
-client = bigquery.Client.from_service_account_json(KEY_FILE)
-
-def create_table(project_id=None, dataset_name=None, table_name=None):
-    project_id = project_id or os.getenv("PROJECT_ID")
-    dataset_name = dataset_name or os.getenv("DATASET_NAME")
-    table_name = table_name or os.getenv("TABLE_NAME")
-
+def create_table(project_id, dataset_name, table_name):
     dataset_id = f"{project_id}.{dataset_name}"
     table_id = f"{dataset_id}.{table_name}"
 
-    logging.info(f"Creating table '{table_name}' in dataset '{dataset_name}' under project '{project_id}'...")
+    logging.info(f"Creating table '{table_name}' in dataset '{dataset_name}' of project '{project_id}'...")
 
     schema = [
         bigquery.SchemaField("ID", "INTEGER", mode="REQUIRED"),
@@ -44,16 +32,15 @@ def create_table(project_id=None, dataset_name=None, table_name=None):
     table = bigquery.Table(table_id, schema=schema)
 
     try:
-        # Check if table already exists
         client.get_table(table_id)
-        logging.info(f"Table '{table_name}' already exists in dataset '{dataset_name}'.")
+        logging.info(f"Table '{table_name}' already exists.")
         return None
     except NotFound:
-        # Table does not exist, proceed to create
         try:
             created_table = client.create_table(table)
             logging.info(f"Table '{table_name}' created successfully.")
             return created_table
         except Exception as e:
-            logging.error(f"An error occurred while creating the table: {e}")
+            logging.error(f"Error creating table: {e}")
             return None
+
