ca0c968c09c1a323cd0c260aa836c3908f2a7908
RealElvince <obuongely@yahoo.com>
2025-06-23 13:12:51 +0300

    improve bucket cration fucntion

--- buckets/gcs_bucket.py
+++ buckets/gcs_bucket.py
@@ -1,35 +1,31 @@
 from google.cloud import storage
-from dotenv import load_dotenv
-import os
 import logging
 
-load_dotenv()
 key_file = "/opt/airflow/gcp/service_account.json"
 client = storage.Client.from_service_account_json(key_file)
 
-def create_bucket(bucket_name=None):
-    if bucket_name is None:
-        bucket_name = os.getenv("BUCKET_NAME")
-
-    logging.info(f"Attempting to create GCS bucket: {bucket_name}")
+def create_bucket(bucket_name, location='US', storage_class='STANDARD'):
+    logging.info(f"Attempting to create GCS bucket: {bucket_name} in location '{location}' with storage class '{storage_class}'")
 
     try:
+        # Check if bucket already exists
         existing_buckets = [bucket.name for bucket in client.list_buckets()]
         if bucket_name in existing_buckets:
             logging.info(f"Bucket '{bucket_name}' already exists.")
             return client.get_bucket(bucket_name)
 
+        # Create new bucket
         bucket = client.bucket(bucket_name)
-        bucket.storage_class = "STANDARD"
-        new_bucket = client.create_bucket(bucket, location="US")
-        logging.info(f"Bucket '{bucket_name}' created successfully in location 'US' with STANDARD storage class.")
+        bucket.storage_class = storage_class
+        new_bucket = client.create_bucket(bucket, location=location)
 
+        # Enable versioning
         new_bucket.versioning_enabled = True
         new_bucket.patch()
-        logging.info("Bucket versioning enabled.")
 
+        logging.info(f"Bucket '{bucket_name}' created successfully and versioning enabled.")
         return new_bucket
 
     except Exception as e:
-        logging.error(f"Error creating bucket '{bucket_name}': {e}")
+        logging.error(f"An error occurred while creating the bucket: {e}")
         return None
