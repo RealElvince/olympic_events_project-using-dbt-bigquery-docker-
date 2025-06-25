d97158877f2075fb2f2bc5ca9bd29ee9930e7ff1
RealElvince <obuongely@yahoo.com>
2025-06-25 19:35:53 +0300

    refactor macro

--- dbt/athletes_project/macros/metrics.sql
+++ dbt/athletes_project/macros/metrics.sql
@@ -1,58 +1,58 @@
+-- macros/metrics.sql
 
- -- number of males
-{{% macro count_males(source_table)% }}
- SELECT 
-   COUNT(*) AS number_of_males
-FROM {{ref(source_table)}}
-WHERE Sex="M"
-{{% endamcro%}}
+-- Number of males
+{% macro count_males(source_table) %}
+    SELECT 
+        COUNT(*) AS number_of_males
+    FROM {{ ref(source_table) }}
+    WHERE Sex = 'M'
+{% endmacro %}
 
--- number of females
-{{% macro count_females(source_table)% }}
- SELECT 
-   COUNT(*) AS number_of_sfemales
-FROM {{ref(source_table)}}
-WHERE Sex="F"
-{{% endamcro%}}
+-- Number of females
+{% macro count_females(source_table) %}
+    SELECT 
+        COUNT(*) AS number_of_females
+    FROM {{ ref(source_table) }}
+    WHERE Sex = 'F'
+{% endmacro %}
 
--- number of sports
+-- Number of unique sports
 {% macro count_unique_sports(source_table) %}
-    SELECT COUNT(DISTINCT Sport) AS total_sports
+    SELECT 
+        COUNT(DISTINCT Sport) AS total_sports
     FROM {{ ref(source_table) }}
 {% endmacro %}
 
--- numbers of events
+-- Number of unique events
 {% macro count_unique_events(source_table) %}
-    SELECT COUNT(DISTINCT Event) AS total_events
+    SELECT 
+        COUNT(DISTINCT Event) AS total_events
     FROM {{ ref(source_table) }}
 {% endmacro %}
 
--- numbers of teams
+-- Number of unique teams
 {% macro count_unique_teams(source_table) %}
-    SELECT COUNT(distinct Team) AS total_teams
+    SELECT 
+        COUNT(DISTINCT Team) AS total_teams
     FROM {{ ref(source_table) }}
 {% endmacro %}
 
---number of athletes with or no medal
--- macros/metric.sql
-
+-- Athletes with medals (Gold, Silver, Bronze)
 {% macro athlete_with_medals(source_table, medal_gold, medal_silver, medal_bronze) %}
-
-SELECT
-  athlete_id,
-  Name AS athlete_name,
-  COUNTIF(Medal = {{ medal_gold }}) AS gold_won,
-  COUNTIF(Medal = {{ medal_silver }}) AS silver_won,
-  COUNTIF(Medal = {{ medal_bronze }}) AS bronze_won
-FROM {{ source_table }}
-GROUP BY athlete_id,athlete_name
-
+    SELECT
+        ID AS athlete_id,
+        Name AS athlete_name,
+        COUNTIF(Medal = {{ medal_gold }}) AS gold_won,
+        COUNTIF(Medal = {{ medal_silver }}) AS silver_won,
+        COUNTIF(Medal = {{ medal_bronze }}) AS bronze_won
+    FROM {{ ref(source_table) }}
+    GROUP BY athlete_id, athlete_name
 {% endmacro %}
 
---number of athletes with no medals
-{{% macro number_of_athletes_with_no_medal(source_table)%}}
-   SELECT
-      COUNT(*) AS number_of_athletes_with_no_medals
-   FROM {{source_table}}
-   WHERE Medal IS NULL 
-{{%endamcro%}}
+-- Number of athletes with no medal
+{% macro number_of_athletes_with_no_medal(source_table) %}
+    SELECT
+        COUNT(*) AS number_of_athletes_with_no_medals
+    FROM {{ ref(source_table) }}
+    WHERE Medal IS NULL 
+{% endmacro %}
